4e5da5e4da618e914b0b7354099da21d752c18a5
Balram Rajbanshi <balramrajbanshi4769@gmail.com>
2025-05-22 19:03:25 +0545

    remove commented

--- src/components/StyledComponent/ContactCard.jsx
+++ src/components/StyledComponent/ContactCard.jsx
@@ -93,29 +93,6 @@ const ContactCard = () => {
   };
 
   return (
-    // <StyledWrapper>
-    //   <div className="form-card1">
-    //     <div className="form-card2 ">
-    //       <form onSubmit={handleSubmit} className="form">
-    //         <p className="form-heading">Get In Touch</p>
-    //         <div className="form-field">
-    //           <input required placeholder="Name" className="input-field" type="text" />
-    //         </div>
-    //         <div className="form-field">
-    //           <input required placeholder="Email" className="input-field" type="email" />
-    //         </div>
-    //         <div className="form-field">
-    //           <input required placeholder="Subject" className="input-field" type="text" />
-    //         </div>
-    //         <div className="form-field">
-    //           <textarea required placeholder="Message" cols={30} rows={3} className="input-field" defaultValue={""} />
-    //         </div>
-    //         <button className="sendMessage-btn" type='submit'>Send Message</button>
-            
-    //       </form>
-    //     </div>
-    //   </div>
-    // </StyledWrapper>
 
     <StyledWrapper>
       <div className="form-card1">
